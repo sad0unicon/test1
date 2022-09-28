8fa9646af18dd25d55d87b2e125e315cbe7f17a8
UltraCoot <utkin.dima2015@yandex.ru>
2022-09-28 11:18:51 +0300

    Use proper grammar.

---
+++ sueta.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char **argv)
+{
+	char name[255];
+
+	printf("Enter your name: ");
+	fgets(name, 255, stdin);
+	name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+	
+	print("Hello, %s!\n", name);
+	return 0;
+}
